a569648792189a2f6398b4d51c40496b0b187fe7
Waqas Safdar <waqassafdar221@gmail.com>
2025-11-07 02:28:50 +0500

    _again_fix

--- src/pages/_document.tsx
+++
@@ -1,3 +0,0 @@
-// Re-export the root-level pages _document to avoid duplicate document modules
-// @ts-ignore - purposely re-exporting the canonical _document from root pages
-export { default } from '../../../pages/_document.js';
